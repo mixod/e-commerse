637134ab3927b5a2c7b457bb3e8591943cbb8403
Sangram Thapa <thapasangram64@gmail.com>
2025-02-08 15:09:34 +0545

    added

--- src/Content/NAvigation/Navigation.jsx
+++ src/Content/NAvigation/Navigation.jsx
@@ -1,19 +1,30 @@
+import Search from "antd/es/transfer/search";
 import { NavLink } from "react-router-dom";
 function Navigation() {
   return (
     <>
-      <div>
-        <ul>
-          <li>
-            <NavLink to={"/home"}>Home</NavLink>
-          </li>
-          <li>
-            <NavLink to={"/about"}>About</NavLink>
-          </li>
-          <li>
-            <NavLink to={"/contact"}>Contact</NavLink>
-          </li>
-        </ul>
+      <div className="bg-orange-400 p-5 flex flex-col gap-4">
+        <div>
+          <ul className="flex max-w-6xl mx-auto justify-evenly">
+            <li>
+              <NavLink to={"/home"}>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/about"}>About</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/contact"}>Contact</NavLink>
+            </li>
+          </ul>
+          <div className="max-w-3xl mx-auto">
+            <Search
+              placeholder="input search text"
+              enterButton="Search"
+              size="large"
+              loading
+            />
+          </div>
+        </div>
       </div>
     </>
   );
